4eadbdf13faab561eecc280e2dec0cbe589095ec
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-10-28 17:56:49 +0100

    cleanup: dedebug and similar

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const { addPath, debug, error, getInput, setFailed } = require("@actions/core");
-const { downloadTool: download, extractZip } = require("@actions/tool-cache");
+const { addPath, error, getInput, setFailed } = require("@actions/core");
+const { downloadTool: dltmp, extractZip } = require("@actions/tool-cache");
 const latestRelease = require("github-latest-release");
-const { coerce: coerceSemVer, valid: isValidSemVer } = require("semver");
+const { coerce: coerceSemVer } = require("semver");
 const { arch, platform } = require("os");
 const { join } = require("path");
-const { exec: _exec } = require("child_process")
+const { exec: _exec } = require("child_process");
 
 const BASE_URL = "https://github.com/WebAssembly/wabt/releases/download/";
 const PLATFORM = platform();
@@ -19,7 +19,7 @@ if (PLATFORM.startsWith("win")) {
 }
 
 async function exec(cmd) {
-  return new Promise((res, rej) => _exec(cmd, err => err ? rej(err) : res()))
+  return new Promise((res, rej) => _exec(cmd, err => (err ? rej(err) : res())));
 }
 
 function deriveURL(version) {
@@ -37,41 +37,27 @@ async function extract(archive, dir) {
   if (PLATFORM.startsWith("win")) {
     await extractZip(archive, dir);
   } else {
-    await exec(`tar --extract --gunzip --strip-components=1 --file="${archive}" --directory="${dir}"`)
+    await exec(
+      `tar --extract --gunzip --verbose --strip-components=1 --file="${archive}" --directory="${dir}"`
+    );
   }
 }
 
 async function main() {
-  debug(`setup-wabt starting...`);
-
   try {
     let version = coerceSemVer(getInput("version"));
 
     if (!version) {
-      debug("deriving the latest wabt version...");
-
       const release = await latestRelease("WebAssembly", "wabt");
 
       version = release.tag_name.replace(/^v/, "");
     }
 
-    if (!isValidSemVer(version)) {
-      return setFailed(`${version} is not a valid semver`);
-    }
-
-    const url = deriveURL(version);
-
-    debug(`installing wabt from ${url} to ${BIN_DIR}...`);
-
-    const archive = await download(url);
+    const archive = await dltmp(deriveURL(version));
 
     await extract(archive, BIN_DIR);
 
-    // TODO: only if not in PATH
-    debug("manipulating the PATH...");
     addPath(BIN_DIR);
-
-    debug(`wabt-${version} installation successful`);
   } catch (err) {
     error(err.stack);
     setFailed(err.message);
